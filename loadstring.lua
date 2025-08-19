e3532b5eb85368b45d0c6a70b3cf42120444bf38
nfpw <153939922+nfpw@users.noreply.github.com>
2025-08-19 12:21:52 +0300

    Create loadstring.lua

---
+++ loadstring.lua
@@ -0,0 +1,19 @@
+local shared = (getgenv and getgenv()) or shared or _G
+local request = (syn and syn.request) or (http and http.request) or http_request
+shared.Anka = {
+    Config = {
+		Color = Color3.fromRGB(255, 128, 64),
+		Keybind = Enum.KeyCode.RightShift,
+		MinHeight = 100,
+		MaxHeight = 600,
+		InitialHeight = 400,
+		MinWidth = 300,
+		MaxWidth = 800,
+		InitialWidth = 500,
+	},
+}
+
+local req = request({Url = "https://raw.githubusercontent.com/nfpw/XXSCRIPT/refs/heads/main/Games/universal.lua", Method = "GET"})
+if req.StatusCode == 200 then
+    loadstring(req.Body)()
+end
