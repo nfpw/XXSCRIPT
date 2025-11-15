dbc666649ac430ec2172eddb9a3a1317845029e0
nfpw <153939922+nfpw@users.noreply.github.com>
2025-11-15 18:23:30 +0300

    Refactor AdonisBypass.lua for improved detection handling

--- Misc/AdonisBypass.lua
+++ Misc/AdonisBypass.lua
@@ -1,27 +1,35 @@
+local shared = (getgenv and getgenv()) or shared or _G or {}
+shared["Anka"] = shared["Anka"] or {}
+shared["Anka"]["Adonis"] = shared["Anka"]["Adonis"] or {RemoveDetection = true, RemoveClient = false}
+
 local getinfo = getinfo or debug.getinfo
 local trash = getgc(true)
 local shitted = {}
 local detected = nil; local crash = nil
 
-for i, v in next, trash do
-    if typeof(v) ~= "table" then
+if shared["Anka"]["Adonis"]["RemoveDetection"] then
+    for i, v in next, trash do
+        if typeof(v) ~= "table" then
+            continue
+        end
+        if not detected and typeof(rawget(v, "Detected")) == "function" then
+            detected = rawget(v, "Detected")
+            local detect; detect = hookfunction(detected, function(Action, Info, NoCrash) return true end)
+            table.insert(shitted, detected)
+            continue
+        end
+        if not crash and rawget(v, "Variables") and rawget(v, "Process") and typeof(rawget(v, "Kill")) == "function" then
+            crash = rawget(v, "Kill")
+            local nocrash; nocrash = hookfunction(crash, function(Info) end)
+            table.insert(shitted, crash)
+        end
         continue
     end
-    if not detected and typeof(rawget(v, "Detected")) == "function" then
-        detected = rawget(v, "Detected")
-        local detect; detect = hookfunction(detected, function(Action, Info, NoCrash) return true end)
-        table.insert(shitted, detected)
-        continue
-    end
-    if not crash and rawget(v, "Variables") and rawget(v, "Process") and typeof(rawget(v, "Kill")) == "function" then
-        crash = rawget(v, "Kill")
-        local nocrash; nocrash = hookfunction(crash, function(Info) end)
-        table.insert(shitted, crash)
-    end
-    continue
 end
 
-local funnydetect; funnydetect = hookfunction(getrenv().debug.info, newcclosure(function(...)
-    if detected then return coroutine.yield(coroutine.running()); end
-    return funnydetect(...)
-end))
+if shared["Anka"]["Adonis"]["RemoveClient"] then
+    local funnydetect; funnydetect = hookfunction(getrenv().debug.info, newcclosure(function(...)
+        if detected then print("adonis silindi"); return coroutine.yield(coroutine.running()); end
+        return funnydetect(...)
+    end))
+end
